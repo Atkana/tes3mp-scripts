f288e94f105767cb55e67f5a5dacc9d52a09dc6c
Boyos999 <Boyos999@gmail.com>
2019-05-24 13:20:26 -0400

    Update CellReset.lua
    to be consistant with atkana's defaults

--- 0.7/CellReset/CellReset.lua
+++ 0.7/CellReset/CellReset.lua
@@ -4,11 +4,11 @@
 local scriptConfig = {}
 scriptConfig.resetTime = 259200 --The time in (real life) seconds that must've passed before a cell is attempted to be reset. 259200 seconds is 3 days. Set to -1 to disable automatic resetting
 scriptConfig.preserveCellChanges = true --If true, the script won't reset actors that have moved into/from the cell. At the moment, MUST be true.
-scriptConfig.alwaysPreservePlaced = true --If true, the script will always preserve any placed objects, even in cells that it's free to delete from
+scriptConfig.alwaysPreservePlaced = false --If true, the script will always preserve any placed objects, even in cells that it's free to delete from
 
 --Cells entered in the blacklist are exempt from cell resets.
 scriptConfig.blacklist = {
-"Clutter Warehouse - Everything Must Go!",
+--"Pelagiad, Ahnassi's House",
 }
 --Object with the UniqueIndexes entered in this list will be preserved as they were from a cell reset.
 scriptConfig.preserveUniqueIndexes = {
